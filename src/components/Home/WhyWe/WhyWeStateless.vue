a2d135260ce86dfbff588156d1b4c23288481fb3
LuisMunar <luis.emunarr@gmail.com>
2022-05-28 17:21:32 -0500

    Why we section is done

---
+++ src/components/Home/WhyWe/WhyWeStateless.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="d-flex align-items-center justify-content-between p-4">
+    <div :class="`d-flex align-items-start justify-content-center flex-column ${ !isMobile && 'width-5' }`">
+      <h2 class="h3 text-uppercase color-light underline-decoration pb-1 mb-4" id="whyWe">
+        {{ $t('why_we.title') }}
+      </h2>
+
+      <p :class="`h2 color-light mb-4`">
+        {{ $t('why_we.subtitle_one') }} <span class="h2 color-secondary">{{ $t('why_we.subtitle_two') }}</span>
+      </p>
+
+      <p :class="`p-regular color-light mb-4`">
+        {{ $t('why_we.text') }}
+      </p>
+
+      <div class="d-flex align-items-center justify-content-center mb-2">
+        <img src="../../../assets/icons/whyWe/star.svg" alt="star" class="why-we__star mr-1" />
+        <p :class="`p-regular color-light`">
+          {{ $t('why_we.item_one') }}
+        </p>
+      </div>
+
+      <div class="d-flex align-items-center justify-content-center mb-2">
+        <img src="../../../assets/icons/whyWe/star.svg" alt="star" class="why-we__star mr-1" />
+        <p :class="`p-regular color-light`">
+          {{ $t('why_we.item_two') }}
+        </p>
+      </div>
+
+      <div class="d-flex align-items-center justify-content-center mb-2">
+        <img src="../../../assets/icons/whyWe/star.svg" alt="star" class="why-we__star mr-1" />
+        <p :class="`p-regular color-light`">
+          {{ $t('why_we.item_three') }}
+        </p>
+      </div>
+    </div>
+
+    <img v-if="!isMobile" src="../../../assets/images/whyWe/phones.png" alt="phones" class="width-5" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'WhyWeStateless',
+
+  props: {
+    isMobile: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.why-we__star {
+  height: 1.5rem;
+  width: 1.5rem;
+}
+</style>
--- src/components/Home/WhyWe/index.vue
+++ src/components/Home/WhyWe/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <section>
-    Why we
-  </section>
+  <WhyWeStateless :isMobile="isMobile" />
 </template>
+
+<script>
+import WhyWeStateless from './WhyWeStateless.vue'
+
+export default {
+  name: 'WhatDo',
+
+  components: {
+    WhyWeStateless
+  },
+
+  data: () => ({
+    isMobile: false
+  }),
+
+  created() {
+    this.isMobile = window.innerWidth <= 767
+  }
+}
+</script>
