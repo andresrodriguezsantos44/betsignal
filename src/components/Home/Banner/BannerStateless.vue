21e14431ad255bb9807e2249f7807fe30666d83a
LuisMunar <luis.emunarr@gmail.com>
2022-05-28 12:21:22 -0500

    Banner is done

---
+++ src/components/Home/Banner/BannerStateless.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="banner-stateless d-flex alignt-items-center justify-content-center p-4">
+    <div :class="`d-flex alignt-items-center justify-content-center flex-colum ${ isMobile ? 'full-width' : 'middle-width' }`">
+      <h1 class="title text-left color-light mb-3">
+        {{ $t('banner.title_one') }} <span class="title color-secondary">{{ $t('banner.title_two') }}</span> {{ $t('banner.title_three') }}
+      </h1>
+      <p class="subtitle color-light">
+        {{ $t('banner.text') }}
+      </p>
+    </div>
+
+    <img v-if="!isMobile" src="../../../assets/images/banner/world.png" alt="world" class="middle-width" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'BannerStateless',
+
+  props: {
+    isMobile: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.banner-stateless {
+  background-image: url('../../../assets/images/banner/bitcoin.png');
+  background-repeat: no-repeat;
+  background-size: 20rem;
+  background-position: 2rem 1rem;
+}
+</style>
--- src/components/Home/Banner/index.vue
+++ src/components/Home/Banner/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <section>
-    Banner
-  </section>
+  <BannerStateless :isMobile="isMobile" />
 </template>
+
+<script>
+import BannerStateless from './BannerStateless.vue'
+
+export default {
+  name: 'Banner',
+
+  components: {
+    BannerStateless
+  },
+
+  data: () => ({
+    isMobile: false
+  }),
+
+  created() {
+    this.isMobile = window.innerWidth <= 767
+  }
+}
+</script>
