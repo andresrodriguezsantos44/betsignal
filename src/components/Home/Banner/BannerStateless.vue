5ebccdfa51c70366ee1d4fbb43ddef278678eeb4
LuisMunar <luis.emunarr@gmail.com>
2022-05-28 12:48:58 -0500

    Cripto icons were added

--- src/components/Home/Banner/BannerStateless.vue
+++ src/components/Home/Banner/BannerStateless.vue
@@ -4,9 +4,15 @@
       <h1 class="title text-left color-light mb-3">
         {{ $t('banner.title_one') }} <span class="title color-secondary">{{ $t('banner.title_two') }}</span> {{ $t('banner.title_three') }}
       </h1>
-      <p class="subtitle color-light">
+      <p class="subtitle color-light mb-4">
         {{ $t('banner.text') }}
       </p>
+
+      <div class="d-flex align-items-center justify-content-between mt-4">
+        <img src="../../../assets/images/banner/binance.png" alt="binance" class="cripto-logo" />
+        <img src="../../../assets/images/banner/tron.png" alt="tron" class="cripto-logo" />
+        <img src="../../../assets/images/banner/tether.png" alt="tether" class="cripto-logo" />
+      </div>
     </div>
 
     <img v-if="!isMobile" src="../../../assets/images/banner/world.png" alt="world" class="middle-width" />
@@ -32,5 +38,15 @@ export default {
   background-repeat: no-repeat;
   background-size: 20rem;
   background-position: 2rem 1rem;
+}
+
+.cripto-logo {
+  width: 8rem;
+}
+
+@media (max-width: 767px) {
+  .cripto-logo {
+    width: 6rem;
+  }
 }
 </style>
