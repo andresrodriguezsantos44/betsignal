27cdc7ff29012474730bb606e4ab34ce46b9fe1d
LuisMunar <luis.emunarr@gmail.com>
2022-05-29 11:32:01 -0500

    Terms conditions page is done

--- src/components/LegalTerms/TermsConditions/TermsConditionsStateless.vue
+++ src/components/LegalTerms/TermsConditions/TermsConditionsStateless.vue
@@ -1,6 +1,12 @@
 <template>
-  <section>
-    TermsConditionsStateless
+  <section class="d-flex align-items-center flex-column">
+    <img src="../../../assets/images/privacyPolicy/privacy-policy.png" alt="privacy policy" class="width-5 mb-4" />
+    <p class="h1 color-secondary mb-4">{{ $t('terms_conditions.title') }}</p>
+    <p class="p-regular color-gray mb-2">{{ $t('terms_conditions.text_one') }}</p>
+    <p class="p-regular color-gray mb-2">{{ $t('terms_conditions.text_two') }}</p>
+    <p class="p-regular color-gray mb-2">{{ $t('terms_conditions.text_three') }}</p>
+    <p class="p-regular color-gray mb-2">{{ $t('terms_conditions.text_four') }}</p>
+    <p class="p-regular color-gray mb-2">{{ $t('terms_conditions.text_five') }}</p>
   </section>
 </template>
 
