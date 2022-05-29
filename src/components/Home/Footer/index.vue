a962bbfd93f88da426ed5fcda8ad8c6dd4f60880
LuisMunar <luis.emunarr@gmail.com>
2022-05-28 19:36:18 -0500

    Icons back are done

--- src/components/Home/Footer/index.vue
+++ src/components/Home/Footer/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <footer>
-    Footer
+  <footer class="footer bg-secondary color-light">
+    COPYRIGHT 2022 BETSIGNAL. {{ $t('footer.text') }}
   </footer>
 </template>
+
+<style>
+.footer {
+  padding: 1rem 6rem;
+}
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-primary">
-    <div class="default-layout">
+    <div class="default-layout icons">
       <Navbar />
       <router-view></router-view>
-      <Footer />
     </div>
+    <Footer />
   </div>
 </template>
 
@@ -26,11 +26,10 @@ export default {
 .default-layout {
   width: 100%;
   margin: auto;
-
-  background-image: url('../assets/images/background/bitcoin.png'), url('../assets/images/background/tether.png');
-  background-repeat: no-repeat, no-repeat;
-  background-size: 20rem, 20rem;
-  background-position: 2% 4rem, 98% 45rem;
+  background-image: url('../assets/images/background/bitcoin.png'), url('../assets/images/background/tether.png'), url('../assets/images/background/bitcoin-2.png'), url('../assets/images/background/bc-1.png'), url('../assets/images/background/bc-2.png'), url('../assets/images/background/btc-small.png'), url('../assets/images/background/bc-2.png'), url('../assets/images/background/tether-small.png'), url('../assets/images/background/bc-1.png'), url('../assets/images/background/bc-2.png');
+  background-repeat: no-repeat, no-repeat, no-repeat, no-repeat, no-repeat, no-repeat, no-repeat, no-repeat, no-repeat;
+  background-size: 20rem, 20rem, 20rem, 6rem, 4rem, 4rem, 4rem, 4rem, 4rem, 3rem;
+  background-position: 2% 4rem, 98% 45rem, 2% 70rem, 2% 60rem , 98% 77rem, 2% 135rem, 50% 130rem, 98% 140rem, 2% 170rem, 98% 180rem;
 }
 
 @media (min-width: 1024px) {
